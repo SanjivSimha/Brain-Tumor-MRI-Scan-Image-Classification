ac1f14fc96e8ddebd3c127fd5d4f447a2cf3ba61
Sanjiv Simha <sanjivsimha@eduroam-169-233-229-47.ucsc.edu>
2025-02-03 17:09:40 -0800

    finished adding glioma test images

--- braintumorMLproject.py
+++ braintumorMLproject.py
@@ -2,4 +2,14 @@ from PIL import Image
 import os
 import numpy as np
 
-dataset_path = "path/to/dataset"
+folder_path = 'braindataset/Testing/glioma'
+
+images_training_glioma = []
+
+for filename in os.listdir(folder_path):
+    if filename.endswith(('.png', '.jpg', '.jpeg', '.bmp', '.gif')):
+        file_path = os.path.join(folder_path, filename)
+        img = Image.open(file_path)
+        images_training_glioma.append(img)
+
+print(images_training_glioma)
